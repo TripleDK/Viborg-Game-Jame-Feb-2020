24844411c6bcdd0a735af422c4a57338b04b1d28
Dennis Laptop <mail@gmail.com>
2020-02-08 13:15:53 +0100

    Folderstructure and begin work on light / shadows

---
+++ Assets/Characters/MainCharacter/WerewolfStateController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WerewolfStateController : MonoBehaviour
+{
+
+    public bool wolfForm = false;
+
+    [Header("Human stats")]
+    [SerializeField]
+    private float humanSpeed;
+    [SerializeField]
+    private float humanJump;
+    [SerializeField]
+    private float humanStrength;
+
+    [Header("Wolf stats")]
+    [SerializeField]
+    private float wolfSpeed;
+    [SerializeField]
+    private float wolfJump;
+    [SerializeField]
+    private float wolfStrength;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("Hello");
+
+    }
+ 
+}
