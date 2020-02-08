4360cabc93498278ce034232faf9704c1fbd05c5
Dennis Laptop <mail@gmail.com>
2020-02-08 14:34:33 +0100

    work on playercontroller

--- Assets/Characters/Main Character/WerewolfStateController.cs
+++ Assets/Characters/Main Character/WerewolfStateController.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets;
+using UnityStandardAssets._2D;
 using UnityEngine.UI;
+using UnityEditor.Animations;
 
 public class WerewolfStateController : MonoBehaviour
 {
-
     public bool wolfForm = false;
 
     [Header("Human stats")]
@@ -15,6 +17,8 @@ public class WerewolfStateController : MonoBehaviour
     private float humanJump;
     [SerializeField]
     private float humanStrength;
+    [SerializeField]
+    private AnimatorController humanAnimator;
 
     [Header("Wolf stats")]
     [SerializeField]
@@ -23,11 +27,65 @@ public class WerewolfStateController : MonoBehaviour
     private float wolfJump;
     [SerializeField]
     private float wolfStrength;
+    [SerializeField]
+    private AnimatorController wolfAnimator;
+
+    private PlatformerCharacter2D playerController;
+    private Animator anim;
+    private bool touchingShadow = false;
+    private bool touchingLight = false; 
+
+    void Start()
+    {
+        playerController = GetComponent<PlatformerCharacter2D>();
+        anim = transform.Find("Graphics").GetComponent<Animator>();
+
+
+        if (wolfForm) TransformToWolf();
+        else TransformToHuman();
+    }
 
     void OnTriggerEnter2D(Collider2D collider)
     {
-        Debug.Log("Hello");
-
+        if (collider.gameObject.tag == "Light")
+        {
+            touchingLight = true;
+            TransformToWolf();
+        }
+        else if (collider.gameObject.tag == "Shadow")
+        {
+            touchingShadow = true;
+            TransformToHuman();
+        }
     }
- 
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Light")
+        {
+            touchingLight = false;
+         }
+        else if (collider.gameObject.tag == "Shadow")
+        {
+            touchingShadow = false;
+            TransformToHuman();
+        }
+    }
+
+    void TransformToWolf()
+    {
+        wolfForm = true;
+        playerController.m_MaxSpeed = wolfSpeed;
+        playerController.m_JumpForce = wolfJump;
+        anim.runtimeAnimatorController = wolfAnimator;
+    }
+    
+
+    void TransformToHuman()
+    {
+        wolfForm = false;
+        playerController.m_MaxSpeed = humanSpeed;
+        playerController.m_JumpForce = humanJump;
+        anim.runtimeAnimatorController = humanAnimator;
+    }
 }
