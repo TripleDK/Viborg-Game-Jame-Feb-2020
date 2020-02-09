41ec04b6adf01d39b1ccb09cf8d4c99f34fc9eab
Suinotna <antonius.marcussen@gmail.com>
2020-02-09 04:00:12 +0100

    dekoreret level2. stadig en del mangler. de nye 2D lights skal fungere sammen med wolf->man morph mechanicen

--- Assets/ParallaxEffect1.cs
+++ Assets/ParallaxEffect1.cs
@@ -8,20 +8,26 @@ public class ParallaxEffect1 : MonoBehaviour
 
     public float scrollingFactor = 2f;
 
+    public bool freezeY = false;
+
     Transform cameraTransform;
     Vector3 ownStartPosition;
     Vector3 offset;
+    Vector3 cameraStartPosition;
 
     void Start()
     {
         cameraTransform = Camera.main.transform;
         ownStartPosition = transform.position;
         offset = transform.position - Camera.main.transform.position;
+        cameraStartPosition = Camera.main.transform.position;
     }
 
 
     void LateUpdate()
     {
+        if (!freezeY)
+        { 
         if (staticBackground)
         {
             transform.position = new Vector3(cameraTransform.position.x, cameraTransform.position.y, ownStartPosition.z);
@@ -29,8 +35,11 @@ public class ParallaxEffect1 : MonoBehaviour
         else
         {
             transform.position = new Vector3(cameraTransform.position.x / scrollingFactor, cameraTransform.position.y / scrollingFactor, ownStartPosition.z) + offset;
-
-
+        }
+        }
+        if(freezeY)
+        {
+            transform.position = new Vector3(cameraTransform.position.x / scrollingFactor + offset.x, ownStartPosition.y, ownStartPosition.z);
         }
     }
 }
